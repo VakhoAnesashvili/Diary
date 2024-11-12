712d2519b0d213ac5f619c2ff6cbc1f2de3d4252
Mariamchavchavadze <94233639+Mariamchavchavadze@users.noreply.github.com>
2024-11-12 18:28:56 +0400

    Update urls.py

--- my_diary/diary_notes/urls.py
+++ my_diary/diary_notes/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import DiaryEntryListCreateView, DiaryEntryRetrieveView, DiaryEntryUpdateView, DiaryEntryDestroyView
+from .views import DiaryEntryListCreateView, DiaryEntryRetrieveView, DiaryEntryUpdateView, DiaryEntryDestroyView,generating_code
 
 urlpatterns = [
     path('entries/', DiaryEntryListCreateView.as_view(), name='entry-list-create'),
     path('entries/<int:pk>/', DiaryEntryRetrieveView.as_view(), name='entry-retrieve'), 
     path('entries/update/<int:pk>/', DiaryEntryUpdateView.as_view(), name='entry-update'),  
-    path('entries/delete/<int:pk>/', DiaryEntryDestroyView.as_view(), name='entry-destroy'),  
+    path('entries/delete/<int:pk>/', DiaryEntryDestroyView.as_view(), name='entry-destroy'), 
+    
+    #redis part
+    path('generate-code/', generating_code, name='generate_code') 
 ]
